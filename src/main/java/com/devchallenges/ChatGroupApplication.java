80db2ccd05aa9373a306b042f7458c747f994540
bsheely <brian.sheely@gmail.com>
2022-07-15 16:57:29 -0700

    Initial commit

---
+++ src/main/java/com/devchallenges/ChatGroupApplication.java
@@ -0,0 +1,13 @@
+package com.devchallenges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatGroupApplication.class, args);
+	}
+
+}
